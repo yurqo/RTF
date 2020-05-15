7a58209dd5d0d204ed05258014389a8f08273c2b
Yurko <yuko@perekupko.net>
2020-05-16 01:45:01 +0800

    Init

---
+++ RTF/RTF/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RTF
+//
+//  Created by Yuriy Perekupko on 16.05.2020.
+//  Copyright © 2020 Yurko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
